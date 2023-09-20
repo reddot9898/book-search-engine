ac3e2616d8a81bfeaf4cff47706bca9edb588146
randyn.collins@icloud.com <you@example.com>
2023-09-20 09:07:14 -0400

    finished code for database, code is working properlyu just need to finish read me

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://root:root@cluster0.4brnzrd.mongodb.net/booksearchDB');
 
 module.exports = mongoose.connection;
